5f7bb6fa466a62231aa3b914bb2904ae2a80f2f0
Ubuntu <ubuntu@sospinah-vd01.novalocal>
2022-03-03 00:29:33 -0400

    configuration memory sonarqube

--- TestDocker.sh
+++ TestDocker.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/bash
 
 # Folders Postgresql
-mkdir ~/postgresql && mkdir ~/postgresql_data
- 
+# mkdir ~/postgresql && mkdir ~/postgresql_data
+
+# Config for sonarqube
+sysctl -w vm.max_map_count=524288
+sysctl -w fs.file-max=131072
+ulimit -n 131072
+ulimit -u 8192
+
 # Docker volumes postgres
 docker volume create postgresql
 docker volume create postgresql_data
@@ -13,6 +19,9 @@ docker volume create sonarqube_data
 docker volume create sonarqube_extensions
 docker volume create sonarqube_logs
 
+# Network
+docker network create atnet
+
 # Docker jenkins
 docker run -d --name myjenkins \
 --network atnet -p 8080:8080 -p 50000:50000 \
@@ -22,7 +31,7 @@ jenkins/jenkins:lts
 #docker postgres
 docker run -d --name sonardb \
 --network atnet --restart always \
--e POSTGRES_USER=sonar -e POSTGRES_PASSWORD=sonar \
+-e POSTGRES_USER=sonar -e POSTGRES_PASSWORD=sonaradmin \
 -v postgresql:/var/lib/postgresql \
 -v postgresql_data:/var/lib/postgresql_data \
 postgres:12.1-alpine
@@ -30,12 +39,11 @@ postgres:12.1-alpine
 # Docker Sonarqube
 docker run -d --name sonarqube \
 --network atnet -p 9000:9000 \
--e SONARQUBE_JDBC_URL=jdbc:postgresql://sonardb:5432/sonar \
+-e SONARQUBE_JDBC_URL=jdbc:postgresql://sonardb/sonar \
 -e SONAR_JDBC_USERNAME=sonar \
 -e SONAR_JDBC_PASSWORD=sonaradmin \
 -v sonarqube_data:/opt/sonarqube/data \
 -v sonarqube_extensions:/opt/sonarqube/extensions \
 -v sonarqube_logs:/opt/sonarqube/logs \
 sonarqube:8.9.0-community
-
 
